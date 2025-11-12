6e3c72129bd6a750669ab38e2e60f76988cacb48
nourinerasmy16006612 <nourine.rasmy@student.giu-uni.de>
2025-11-12 15:54:43 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,15 +1,16 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
+#include "Animal.h"
 #include <iostream>
 #include <string>
 using namespace std;
 
 class Mammal : public Animal{
     private :
-    string furColor
+    string furColor;
     public : 
     Mammal();
     Mammal(string n,int a,bool h,string fur);
-    ~Mammal():
+    ~Mammal();
 };
 #endif
