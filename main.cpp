5f9af9319f690a4ad809c7431e92ce19d4a8e5fb
nourinerasmy16006612 <nourine.rasmy@student.giu-uni.de>
2025-11-12 15:42:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,101 @@
 #include "Animal.h"
-#include "Bird.h"
 #include "Mammal.h"
+#include "Bird.h"
 #include "Reptile.h"
 #include "Enclosure.h"
 #include "Visitor.h"
+
+
 #include <iostream>
+#include <string>
 using namespace std;
 
+ Animal:: Animal(){
+     
+ }
+    Animal:: Animal(string n,int a,bool h){
+        this->name = n;
+        this->age=a;
+        this->isHungry=h;
+    }
+    Animal:: ~Animal(){}
+    void Animal:: display(){
+        cout<< "Name: "<< this->name<<endl;
+         cout<< "Age: "<< this->age<<endl;
+        cout<< "Is hungry: "<< this->isHungry<<endl;
 
-// ============== MAIN FUNCTION ==============
-int main() {
+    }
+    void Animal:: feed(){
+        if(isHungry)isHungry=false;
+    }
+    string Animal:: getName(){
+        return this->name;
+    }
+    int Animal:: getAge(){
+        return this->age;
+    }
+    bool Animal:: isHungry(){
+        return this->isHungry;
+    }
     
+    void Animal::  setName(string n){
+        this->name=n;
+    }
+    void Animal:: setAge(int a){
+        this->age=a;
+    }
+    void Animal:: setH(bool h){
+        this->isHungry=h;
+    }
     
-    return 0;
-}
+    Mammal:: Mammal():Animal(){}
+    Mammal:: Mammal(string n,int a,bool h,string fur):Animal(n,a,h){
+        this->furColor = fur;
+    }
+ Mammal::    ~Mammal(){}
+ 
+ Bird::  Bird(){}
+   Bird::  Bird(string n, int a, bool h, float w):Animal(n,a,h){
+       this->wingSpan =w;
+   }
+  Bird::  ~Bird(){
+  }
+  
+  Reptile:: Reptile(){}
+   Reptile::  Reptile(string n,int a,bool h, bool ven):Animal(n,a,h){
+       this->isVenomous=ven;
+   }
+   Reptile::  ~Reptile(){}
+   
+ Enclosure::   Enclosure(){}
+    Enclosure:: Enclosure(int cap,int count){
+        this->capacity =cap;
+        this->currentCount=count;
+        this->animals = (Animal*)malloc(cap*sizeof(Animal));
+    }
+   Enclosure:: ~Enclosure(){}
+    void Enclosure:: addAnimals(Animal* a){
+        if(animals!=nullptr){
+            (animals+currentCount)=a;
+        }
+    }
+    void Enclosure:: displayAnimals(){}
+    int Enclosure:: getCap(){return this->capacity;}
+    int Enclosure:: getCount(){return this->currentCount;}
+    void Enclosure:: setCap(int c){ this->capacity=c;}
+    void Enclosure:: setCount(int c){ this->currentCount=c;}
+   
+   Visitor:: Visitor(){}
+  Visitor::  Visitor(string v,int t){
+      this->visitorName = v;
+      this->ticketsBought=t;
+  }
+    Visitor:: ~Visitor(){}
+    void Visitor:: diplayInfo(){}
+    string Visitor:: getVName(){return this->visitorName;}
+    int Visitor:: getTB(){return this->ticketsBought;}
+    void Visitor:: setVName(string n){this->visitorName=n;}
+    void Visitor:: setTB(int t){this->ticketsBought=t}
+    
+    int main(){}
+    
