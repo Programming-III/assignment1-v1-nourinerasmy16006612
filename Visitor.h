247addae3f3433876fdaa1cad67ab39d8d67113a
nourinerasmy16006612 <nourine.rasmy@student.giu-uni.de>
2025-11-12 15:56:19 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,25 @@
-#ifndef VISITOR_H
+#ifndef VISITOR_H 
 #define VISITOR_H
+#include "Animal.h"
 
+#include <iostream>
 #include <string>
+//include animal.h?
 using namespace std;
 
-//define visitor class here
-
-
-
-
-
-
-
+class Visitor{
+    private :
+    string visitorName;
+    int ticketsBought;
+    public:
+    Visitor();
+    Visitor(string v,int t);
+    ~Visitor();
+    void diplayInfo();
+    string getVName();
+    int getTB();
+    void setVName(string n);
+    void setTB(int t);
+}
+;
 #endif
