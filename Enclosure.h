f61f08c1f503fbb440cdc54ba9c5ce52a34f1343
nourinerasmy16006612 <nourine.rasmy@student.giu-uni.de>
2025-11-12 15:10:49 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,19 @@
 #ifndef ENCLOSURE_H
 #define ENCLOSURE_H
+#include <iostream>
+#include <string>
+//include animal.h?
+using namespace std;
 
-#include "Animal.h"
-
-//define enclosure class here 
-
-
-
-
-
-
-
-
+class Enclosure{
+    private:
+    int capacity;
+    int currentCount;
+    Animal* animals;
+    public:
+    Enclosure();
+    Enclosure(int cap,int count);
+    void addAnimals(Animal* a);
+    void displayAnimals();
+};
 #endif
