d4f1ec254133896ee2496382c6f6cec62e4acb2c
nourinerasmy16006612 <nourine.rasmy@student.giu-uni.de>
2025-11-12 15:55:00 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,11 +1,17 @@
 #ifndef BIRD_H
 #define BIRD_H
 #include <iostream>
+#include "Animal.h"
+
 #include <string>
 using namespace std;
 class Bird : public Animal{
     private :
     float wingSpan;
+    public:
+    Bird();
+    Bird(string n, int a, bool h, float w);
+    ~Bird();
     
 };
 
