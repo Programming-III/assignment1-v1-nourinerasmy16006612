8518c34c253eabe9512c73c7897d9a155a8759e9
nourinerasmy16006612 <nourine.rasmy@student.giu-uni.de>
2025-11-12 14:54:39 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,23 @@
 #ifndef ANIMAL_H
-#define ANIMAL_H
+define ANIMAL_H
 
+#include <iostream>
 #include <string>
 using namespace std;
-
-//define animal class here 
-
+class Animal{
+    private:
+    string name;
+    int age;
+    bool isHungry;
+    
+    public:
+    Animal();
+    Animal(string n,int a,bool h);
+    ~Animal();
+    void display();
+    void feed();
+    
+};
 
 
 #endif
